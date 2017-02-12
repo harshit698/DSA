599e2ad69a2f6fc6409e8e259378ae32cdcd6ecf
saurav kumar  bhagat <sauravbhagat6969@gmail.com>
2017-02-12 11:34:46 +0530

    Add files via upload
    There was an  error in previous code. It was showing positive result in those cases also, when symbols were left in the stack.
    So, this code removes that error.

--- task3.c
+++ task3.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
 #include<ctype.h>
 #include<string.h>
+
+
 void main()
 {
-	char code[100];
+	char code[100];          //code array will hold the code snippet;
 	int i=0,flag=2,top=-1;
-	char stack[50];
+	char stack[50];            //stack array if for storing brackets(symbols);
+	
+	
 	printf("Enter the code snippet:\n");
 	scanf("%[^?]s",code);
+	
+	
 	while(code[i]!='\0')
 	{
 		if(code[i]=='('||code[i]=='['||code[i]=='{')
@@ -63,11 +69,14 @@ void main()
 				}
 			}
 		}
-		else
+		else   //this condition is for all characters except brackets.
 		i++;
 	}
+	if(top!=-1)  //finally, stack holding symbols must be empty for balancing all symbols;
+		flag=3;
+		
 	if(flag==2)
-	printf("Everything is correct.");
+		printf("Everything is correct.");
 	else if(flag==3)
-	printf("Unbalalnced expression.\n");
+		printf("Unbalalnced expression.\n");
 }
