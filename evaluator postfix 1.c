efb089a7d7fa7cb3955529d8773191cde4b6e3ae
saurav kumar  bhagat <sauravbhagat6969@gmail.com>
2017-03-06 21:17:23 +0530

    Add files via upload
    This code takes infix expression as input and evaluates it by first converting it into postfix , using 2 stacks only

---
+++ evaluator postfix 1.c
@@ -0,0 +1,166 @@
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+
+char oprstack[50];     //stack1
+int evalstack[50];   //stack2
+int top1=-1,top2=-1;
+
+void pushopr(char k)
+{
+	oprstack[++top1]=k;
+}
+
+void pusheval(int k)
+{
+	evalstack[++top2]=k;
+}
+
+char popopr()
+{
+	return(oprstack[top1--]);
+	
+}
+
+int pr(char ch)
+{
+	switch(ch)
+	{
+		case '+':
+		case '-':
+			return 1;
+		
+		case '*':
+		case '/':
+			return 3;
+			
+		case '(':
+			return 0;
+			
+			
+	}
+}
+
+
+
+
+
+
+
+int main()
+{
+	
+	char infix[50],k;
+	char ch;
+	int i=0,c;
+	
+	
+	printf("Enter the expression: \n");
+	scanf("%s",infix);                                      //we can convert string to integer with either "atoi(n)"  or  x-'0'.
+	
+	
+	while((ch=infix[i++])!='\0')
+	{
+		if(ch=='(')
+			pushopr(ch);
+			
+		else if(isdigit(ch))
+		{
+			pusheval(ch-'0');
+		}
+			
+		else if(ch==')')
+		{
+			while(oprstack[top1]!='(')
+			{
+				k = popopr();    //k = +/-/ * / /.
+				
+				if(k=='+')
+				{
+					c=evalstack[top2-1] + evalstack[top2];
+					top2--;
+					//pusheval(c);
+					printf("%d",c);
+					evalstack[top2]=c;
+				}
+				else if(k=='-')
+				{
+					c=evalstack[top2-1] - evalstack[top2];
+					top2--;
+					//pusheval(c);
+					evalstack[top2]=c;
+				}
+				else if(k=='*')
+				{
+					c=evalstack[top2-1] * evalstack[top2];
+					top2--;
+					//pusheval(c);
+					evalstack[top2]=c;
+				}
+				else if(k=='/')
+				{
+					c=evalstack[top2-1] / evalstack[top2];
+					top2--;
+					//pusheval(c);
+					evalstack[top2]=c;
+				}
+				
+			}
+			k=popopr();
+		}
+		else
+		{
+			if(top1==-1)
+			{
+				pushopr(ch);
+			}
+	
+			else
+			{
+				if(pr(oprstack[top1])>=pr(ch))
+				{
+					k=popopr();
+				
+					if(k=='+')
+					{
+						c=evalstack[top2-1] + evalstack[top2];
+						top2--;
+						//pusheval(c);
+						printf("\n inside block c is : %d\n",c);
+						evalstack[top2]=c;
+					}
+					else if(k=='-')
+					{
+						c=evalstack[top2-1] - evalstack[top2];
+						top2--;
+						//pusheval(c);
+						evalstack[top2]=c;
+					}
+					else if(k=='*')
+					{
+						c=evalstack[top2-1] * evalstack[top2];
+						top2--;
+						//pusheval(c);
+						evalstack[top2]=c;
+					}
+					else if(k=='/')
+					{
+						c=evalstack[top2-1] / evalstack[top2];
+						top2--;
+						//pusheval(c);
+						evalstack[top2]=c;
+					}
+				
+			    }
+			    pushopr(ch);
+				
+			}
+		
+			
+		}
+	}
+	printf("\nOutput is : %d",evalstack[top2]);
+	
+	
+	return 0;
+}
